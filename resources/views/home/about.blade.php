c35e72799347cd4de7034f4aed31d88001aae3c6
dilarakiraz <dilarakiraz12345@gmail.com>
2022-10-14 17:09:07 +0300

    first commit

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.home')
+
+@section('title', 'About Us')
+
+@section('title','About Us')
+
+@section('content')
+    <div class="col-sm-12">
+        <h1>About Us</h1>
+        aaaaaaaaaaaa
+        <br>
+        bbbbbbbbbbbbbbbbbbbbb<br>
+    </div>
+@endsection
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/home2', function () {
+    return view('welcome');
+});
+
+Route::redirect('/anasayfa','/home')->name('anasayfa');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('aboutus');
+
+Route::get('/',function (){
+    return view('home.index');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
